7defc3cc85acb132a157619152b6072a137697b2
Lin <2295141009@stu.hqu.edu.cn>
2023-12-13 11:34:42 +0800

    添加字体

--- src/views/Home-View.vue
+++ src/views/Home-View.vue
@@ -1,19 +1,17 @@
 <template>
-    <div id="home-container" ref="containerRef" :class="campas" @click="next">
-        <img src="@/assets/img/home/home-xiamen.jpg" />
+    <div id="home-container" ref="containerRef" :class="settingStore.campus" @click="next">
         <fade-transition id="tip">点击任意处继续!</fade-transition>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { type Campus } from '@/models/modules/user/type/index'
 import FadeTransition from '@/components/Fade-Transition.vue'
 import router from '@/router'
-import { useStatusStore } from '../stores/modules/status'
+import { useStatusStore } from '@/stores/modules/status'
+import { useSettingStore } from '@/stores/modules/setting'
 
 //判断是哪个校区的,展示不同的背景图
-const campas = ref<Campus>('xiamen')
+const settingStore = useSettingStore()
 
 //next事件
 const next = () => {
@@ -25,17 +23,15 @@ useStatusStore().isSettingAvailable = true //开放设置页
 
 <style scoped lang="less">
 #home-container {
-    width: 100%;
-    height: 100%;
-    box-sizing: border-box;
+    width: 100vw;
+    height: 100vh;
     #tip {
         position: absolute;
         left: 0;
         right: 0;
-        bottom: 10%;
+        bottom: 5%;
         z-index: 999;
         text-align: center;
-        font-family: '楷体';
         color: #9ca3af;
         font-size: 24px;
         font-weight: bold;
@@ -46,21 +42,20 @@ useStatusStore().isSettingAvailable = true //开放设置页
 img {
     width: 100%;
     height: 100%;
-    // position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
     margin: auto;
 }
-// .xiamen {
-//     background-image: url('@/assets/img/home/home-xiamen.jpg');
-//     background-size: contain;
-//     background-repeat: no-repeat;
-// }
-// .quanzhou {
-//     background-image: url('@/assets/img/home/home-quanzhou.jpg');
-//     background-size: contain;
-//     background-repeat: no-repeat;
-// }
+.xiamen {
+    background-image: url('../assets/img/home/home-xiamen.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+.quanzhou {
+    background-image: url('../assets/img/home/home-quanzhou.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
 </style>
