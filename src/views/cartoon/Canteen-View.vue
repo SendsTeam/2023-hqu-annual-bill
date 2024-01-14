9c0c59e5bb36630497bd7a5b2bf815a5a289f455
Garfield <2295131008@stu.hqu.edu.cn>
2024-01-14 14:04:31 +0800

    添加食堂提示

--- src/views/cartoon/Canteen-View.vue
+++ src/views/cartoon/Canteen-View.vue
@@ -3,7 +3,7 @@
         v-slot="{ visiableIndex }"
         :effect="unlockNextRoute"
         @click="nextRoute"
-        :max-index="9"
+        :max-index="18"
         next-route-name="map"
     >
         <img
@@ -52,14 +52,72 @@
             src="@/assets/img/cartoon/canteen/canteen-08.png"
             style="z-index: 1"
         />
+      <div class="summary-card">
+        <div class="animate__animated animate__fadeIn" v-if="visiableIndex >= 9">
+                <div class="animate__animated animate__fadeIn" v-show="visiableIndex >= 10">
+                  <n-statistic label="今年你最喜欢的食堂" tabular-nums>
+                    <!-- 这里要做个字体随长度动态缩放 -->
+                      <div
+                          class="animate__animated animate__fadeIn course-name"
+                          v-show="visiableIndex >= 11"
+                      >
+                        {{ userStore.user.paymentStatistic?.restaurant.favorite.name }}
+                      </div>
+
+                            <div
+                                class="animate__animated animate__fadeIn course-name"
+                                v-show="visiableIndex >= 12"
+                            >
+                              <div class="remark ">你在该食堂消费了</div>
+                              <div class="course-name ">{{userStore.user.paymentStatistic?.restaurant.favorite.total }}元</div>
+                            </div>
+                  </n-statistic>
+                  <n-statistic label="你的食堂支出一共是" tabular-nums >
+                    <!-- 这里因为有数值动画所以需要用v-if! -->
+                    <span class="animate__animated animate__fadeIn" v-if="visiableIndex >= 14">
+                            <n-number-animation
+                                :from="0.0"
+                                :precision="2"
+                                :to="userStore.user.paymentStatistic?.restaurant.total"
+                            />
+                        </span>
+                    <span
+                      class="animate__animated animate__fadeIn course-name"
+                      v-show="visiableIndex >= 15"
+                  >元</span
+                  >
+                    <div
+                        class="animate__animated animate__fadeIn course-name"
+                        v-show="visiableIndex >= 16"
+                    >
+                      <div class="remark ">你最早的干饭时间是</div>
+                      <div class="time-name ">{{userStore.user.paymentStatistic?.restaurant.earliestTime }}</div>
+                    </div>
+                    <div
+                        class="animate__animated animate__fadeIn course-name"
+                        v-show="visiableIndex >= 17"
+                    >
+                      <div class="remark ">你最晚的干饭时间是</div>
+                      <div class="time-name ">{{userStore.user.paymentStatistic?.restaurant.latestTime}}</div>
+                    </div>
+
+                  </n-statistic>
+                </div>
+        </div>
+      </div>
+
     </cartoon-player>
+
+
 </template>
 
 <script setup lang="ts">
 import CartoonPlayer from '@/components/Cartoon-Player.vue'
 import { ref } from 'vue'
 import router from '@/router'
-
+import { NStatistic, NNumberAnimation } from 'naive-ui'
+import { useUserStore } from '@/stores/modules/user'
+const userStore = useUserStore()
 //等动画播放完再开放路由
 const isCartoonReady = ref(false)
 //解锁路由
@@ -85,5 +143,31 @@ img {
     right: 0;
     bottom: 0;
     margin: auto;
+}
+
+.summary-card {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  font-size: 10px;
+  width: 129px;
+  height: 250px;
+  top: 57%;
+  left: 53%;
+  z-index: 1;
+  padding: 5px;
+  /*background: coral ;*/
+}
+.remark {
+  color: grey;
+  font-size: 13px;
+}
+.course-name {
+  font-size: 16px;
+
+}
+.time-name {
+  font-size: 13px;
+
 }
 </style>
